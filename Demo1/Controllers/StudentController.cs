c535da605fc2b61b5742ba70090d32533a92ed04
ram <ram.miriyala117@gmail.com>
2019-02-27 08:04:06 +0530

    razor view starting

--- Demo1/Controllers/StudentController.cs
+++ Demo1/Controllers/StudentController.cs
@@ -27,6 +27,40 @@ namespace Demo1.Controllers
             return View(studentList);
         }
 
+        IList<Student> studentList = new List<Student>
+        {
+            new Student() { StudentId=1,StudentName="Ram",Age=22  },
+                new Student() { StudentId=2,StudentName="Surya",Age=29 },
+                new Student() { StudentId=3,StudentName="Ashesh",Age=28},
+                new Student() { StudentId=4,StudentName="Ravi",Age=23},
+                new Student() { StudentId=5,StudentName="Ravi",Age=29 },
+                new Student() { StudentId=6,StudentName="Ashesh",Age=28},
+                new Student() { StudentId=7,StudentName="Ravi",Age=23}
+
+        };
+
+        public ActionResult Edit(int id)
+        {
+
+            var std = studentList.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            //write code to student update
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Learn()
+        {
+            var std = studentList.Where(s => s.StudentId == 1).FirstOrDefault();
+            return View(std);
+        }
   
    
     }
